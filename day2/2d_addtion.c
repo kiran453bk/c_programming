807e76bd305e30d7cda9216c60d4c0fb67415dab
Kiran B Kulkarni <kiran453bk@gmail.com>
2025-12-03 09:46:39 +0530

    initial commit

---
+++ day2/2d_addtion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Input size of matrix
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int A[rows][cols], B[rows][cols], Sum[rows][cols];
+
+    // Input elements of first matrix
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input elements of second matrix
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Perform addition
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Display result
+    printf("Resultant Matrix after Addition:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

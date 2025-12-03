02eb817d59b105d8849bd6c6d97075fd1e581475
Kiran B Kulkarni <kiran453bk@gmail.com>
2025-12-03 09:31:15 +0530

    initial commit

---
+++ day2/2d_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    
+    // Ask user for dimensions
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    
+    int arr[rows][cols];  // Declare 2D array
+    
+    // Input elements
+    printf("Enter elements of the array:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    // Display elements
+    printf("\nThe 2D array is:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
